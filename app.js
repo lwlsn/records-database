9b56d62853eb7065196cddf60d7eb48e826e272c
lwlsn <ewilson01@protonmail.ch>
2020-03-18 17:15:51 +0000

    updates to database to allow text input and deletes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const recordList = document.querySelector('#record-list');
+const form = document.querySelector('#add-record-form');
 
 //create a function to render elements
 function renderRecord(doc) {
@@ -6,27 +7,66 @@ function renderRecord(doc) {
     let li = document.createElement('li');
     let album = document.createElement('span');
     let artist = document.createElement('span');
+    let cross = document.createElement('div');
+    let link = document.createElement('span'); //to do- turn these into hrefs.. 
     
     //set attributes
     li.setAttribute('data-id', doc.id);
     album.textContent = doc.data().album;
     artist.textContent = doc.data().artist;
+    cross.textContent = 'x';
+    link.textContent = doc.data().link;
+
 
     //append data types to each column list
     li.appendChild(album);
     li.appendChild(artist);
+    li.append(cross);
+    li.append(link);
 
     //append master list 
     recordList.appendChild(li);
 
+    //deleting data 
+    cross.addEventListener('click', (evt) => {
+        evt.stopPropagation();
+        let id = evt.target.parentElement.getAttribute('data-id');
+        db.collection('records').doc(id).delete();
+
+    })
+
 }
 
-
+// get data from firebase
 db.collection('records').get().then((snapshot)=> {
    snapshot.docs.forEach(doc => {
        console.log(doc.data());
         renderRecord(doc);
    })
-}); // asynchronous request. can't just store in a var, use then function
+}); 
+
+
+// only get specific data from firebase
+// db.collection('records').where('album', '==', "Petrichor").get().then((snapshot)=> {
+//     snapshot.docs.forEach(doc => {
+//         console.log(doc.data());
+//          renderRecord(doc);
+//     })
+//  }); 
+ 
+
+
+//saving data
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    db.collection('records').add({
+        album: form.album.value,
+        artist: form.artist.value, 
+        link: form.link.value
+    });
+    form.album.value='';
+    form.artist.value='';
+    form.link.value= '';
+})
 
 
